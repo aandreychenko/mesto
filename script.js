39379651e29fdd26134598eeda36ff00eab53d97
Alexander Andreychenko <the-ravenclo@yandex.ru>
2020-11-24 16:46:28 +0300

    adding edit-form opening and closing functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let editForm = document.querySelector('.edit-form');
+let editFormCloseIcon = document.querySelector('.edit-form__close-icon');
+
+//function ShowEditForm() {
+
+//}
+
+function ShowEditForm() {
+	editForm.setAttribute('style', 'display: block');
+}
+
+function HideEditForm() {
+	editForm.setAttribute('style', 'display: none');
+}
+
+profileEditButton.addEventListener('click', ShowEditForm);
+editFormCloseIcon.addEventListener('click', HideEditForm);
