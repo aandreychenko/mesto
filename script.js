2ad18db526b410de74f7fecb6820a84eb1e45585
Alexander Andreychenko <the-ravenclo@yandex.ru>
2020-11-25 22:29:29 +0300

    adding like-button functionality

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+//Включаем возможность открывать и закрывать окно редактирования профиля
+
 let profileEditButton = document.querySelector('.profile__edit-button');
 let editForm = document.querySelector('.edit-form');
 let editFormCloseIcon = document.querySelector('.edit-form__close-icon');
-
-//function ShowEditForm() {
-
-//}
 
 function ShowEditForm() {
 	editForm.setAttribute('style', 'display: block');
