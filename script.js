0400221606738097ea8c70390cd170fc68f8e67e
Alexander Andreychenko <the-ravenclo@yandex.ru>
2020-11-26 01:19:16 +0300

    adding edit-profile popup functionality

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-//Включаем возможность открывать и закрывать окно редактирования профиля
+// Включаем возможность открывать и закрывать окно редактирования профиля
 
 let profileEditButton = document.querySelector('.profile__edit-button');
 let editForm = document.querySelector('.edit-form');
 let editFormCloseIcon = document.querySelector('.edit-form__close-icon');
+let editFormSubmitButton = document.querySelector('.edit-form__submit-button');
+
+let profileName = document.querySelector('.profile__name');
+let profileCaption = document.querySelector('.profile__caption');
+let editFormName = document.querySelector('.edit-form__name');
+let editFormCaption = document.querySelector('.edit-form__caption');
 
 function ShowEditForm() {
 	editForm.setAttribute('style', 'display: block');
+	editFormName.setAttribute('value', profileName.textContent);
+	editFormCaption.setAttribute('value', profileCaption.textContent);
 }
 
-function HideEditForm() {
+function CloseEditForm() {
 	editForm.setAttribute('style', 'display: none');
 }
 
+function SetNewProfileInfo() {
+	profileName.textContent = document.querySelector('.edit-form__name').value;
+	profileCaption.textContent = document.querySelector('.edit-form__caption').value;
+	CloseEditForm();
+}
+
 profileEditButton.addEventListener('click', ShowEditForm);
-editFormCloseIcon.addEventListener('click', HideEditForm);
+editFormCloseIcon.addEventListener('click', CloseEditForm);
+editFormSubmitButton.addEventListener('click', SetNewProfileInfo);
